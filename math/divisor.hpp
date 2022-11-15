71bbfbdf817b05b1384635d0dbacc412856e5403
9tc <38623525+9tc@users.noreply.github.com>
2022-11-15 17:18:39 +0900

    add Divisors

---
+++ math/divisor.hpp
@@ -0,0 +1,13 @@
+#pragma once
+template<class T>
+set<T> getDivisors(T n){
+  set<T> res;
+  for(T i = 1; i * i <= n; ++i){
+    if(n % i == 0){
+      res.insert(i);
+      res.insert(n / i);
+    }
+  }
+  res.erase(n);
+  return res;
+}
