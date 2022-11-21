52de6a82295f416b8f3067c82fe73d8b67d71e12
9tc <38623525+9tc@users.noreply.github.com>
2022-11-21 18:32:34 +0900

    add lis

---
+++ verify/AOJ-DPL_1_D.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_D"
+
+#include "../template/template.hpp"
+#include "../dp/longest-increasing-subsequense.hpp"
+
+int main(){
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  REP(i,n) cin >> a[i];
+  cout << lis(a, true) << endl;
+}
