280e23ccc3a277b88cddd3fbfda466d2ed230b0a
9tc <38623525+9tc@users.noreply.github.com>
2022-12-18 03:48:58 +0900

    add test

---
+++ verify/AOJ-DSL_2_A.test.hpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A"
+
+#include "../template/template.hpp"
+#include "../data-structure/segment-tree/range-minimum-query.hpp"
+
+int main(){
+  int n, q;
+  cin >> n >> q;
+  RangeMinimumQuery rmq(n);
+  REP(i,n) rmq.update(i, INT_MAX);
+  REP(i,q){
+    int com, x, y;
+    cin >> com >> x >> y;
+    if(com == 0) rmq.update(x, y);
+    else cout << rmq.query(x, y) << '\n';
+  }
+}
