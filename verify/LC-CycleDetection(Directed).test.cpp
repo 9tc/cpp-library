275890c0379a26144d5cd2f3bcc07b5af1a6d7b3
9tc <38623525+9tc@users.noreply.github.com>
2023-04-10 00:12:55 +0900

    fix cycle detection test

--- verify/LC-CycleDetection(Directed).test.cpp
+++ verify/LC-CycleDetection(Directed).test.cpp
@@ -1,12 +1,27 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/cycle_detection"
 
-#include "template/template.hpp"
-#include "other/fastio.hpp"
+#include "../template/template.hpp"
+#include "../graph/cycle-detection.hpp"
 
 int main(){
-    int a, b;
-    io.read_i(a);
-    io.read_i(b);
+  int N, M;
+  cin >> N >> M;
+  Graph G(N);
+  map<pair<int,int>, int> edges;
+  REP(i,M){
+    int u, v;
+    cin >> u >> v;
+    G[u].PB(v);
+    edges[{u, v}] = i;
+  }
 
-    io.writeln_i(a + b);
+  auto cycle = detectCycle(G);
+  if(cycle.size()){
+    cout << cycle.size() << endl;
+    REP(i,cycle.size()){
+      cout << edges[{cycle[i], i+1 == cycle.size() ? cycle[0] : cycle[i+1]}] << endl;
+    }
+  }else{
+    cout << -1 << endl;
+  }
 }
