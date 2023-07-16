b08cba864d71dae80352d0f66dad069977c7ee89
9tc <38623525+9tc@users.noreply.github.com>
2023-07-16 15:53:20 +0900

    change directory

---
+++ utils/heuristicUtil.hpp
@@ -0,0 +1,22 @@
+#include "template.hpp"
+std::random_device seed_gen;
+std::mt19937 engine(seed_gen());
+clock_t _startTime;
+double limitTime = -1;
+
+void initTime(){
+    _startTime = clock();
+}
+
+double getTime(){
+    return (double)(clock() - _startTime) / CLOCKS_PER_SEC;
+}
+
+void setLimit(const double limit){
+    limitTime = limit;
+}
+
+bool checkTime(){
+    assert(limitTime != -1);
+    return getTime() <= limitTime;
+}
